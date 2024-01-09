603d88bedb1de9bd522a93cd4b2c2b1ee1275f0c
antur84 <andreas.turku@gmail.com>
2024-01-09 12:16:56 +0100

    feat(main-menu-page.component) new game
    button added, no func

--- src/app/pages/main-menu-page/main-menu-page.component.ts
+++ src/app/pages/main-menu-page/main-menu-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { ButtonComponent } from '@components/button/button.component';
 
 @Component({
   selector: 'app-main-menu-page',
   standalone: true,
-  imports: [],
+  imports: [ButtonComponent],
   templateUrl: './main-menu-page.component.html',
   styleUrl: './main-menu-page.component.scss',
 })
