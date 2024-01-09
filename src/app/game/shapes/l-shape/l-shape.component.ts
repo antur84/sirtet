2b5f96c418e6d20c7dd228d4cc6aef8be067a370
antur84 <andreas.turku@gmail.com>
2024-01-09 16:28:37 +0100

    refactor(game-vars) unitless
    went with unitless vars, then have to multiply
    with 1px to get a unit on them ...

--- src/app/game/shapes/l-shape/l-shape.component.ts
+++ src/app/game/shapes/l-shape/l-shape.component.ts
@@ -6,10 +6,15 @@ import { GameAsset } from 'app/game/game-asset';
   standalone: true,
   imports: [],
   templateUrl: './l-shape.component.html',
-  styleUrl: './l-shape.component.scss'
+  styleUrl: './l-shape.component.scss',
 })
 export class LShapeComponent implements GameAsset {
+  priuvate
   tick = () => {
     console.log('lshapre');
+  };
+
+  move = ({ x, y }: { x: number; y: number }) => {
+    console.log('move', x, y);
   };
 }
