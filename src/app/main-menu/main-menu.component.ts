af6233ca6d7cf8e71b6e9c08939a1fbe4c08fb3c
antur84 <andreas.turku@gmail.com>
2024-01-09 12:16:38 +0100

    feat(main-menu.component) buttons

--- src/app/main-menu/main-menu.component.ts
+++ src/app/main-menu/main-menu.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { ButtonComponent } from '@components/button/button.component';
 
 @Component({
   selector: 'app-main-menu',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, ButtonComponent],
   templateUrl: './main-menu.component.html',
   styleUrl: './main-menu.component.scss'
 })
