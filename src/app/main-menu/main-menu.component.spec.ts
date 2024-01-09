0dae116b7b8d57fb64e668837c6beb1c4ab5828f
antur84 <andreas.turku@gmail.com>
2024-01-09 11:39:32 +0100

    chore(tests) green

--- src/app/main-menu/main-menu.component.spec.ts
+++ src/app/main-menu/main-menu.component.spec.ts
@@ -1,14 +1,15 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MainMenuComponent } from './main-menu.component';
+import { RouterTestingModule } from '@angular/router/testing';
 
-describe('TopMenuComponent', () => {
+describe('MainMenuComponent', () => {
   let component: MainMenuComponent;
   let fixture: ComponentFixture<MainMenuComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [MainMenuComponent]
+      imports: [MainMenuComponent, RouterTestingModule]
     })
     .compileComponents();
 
