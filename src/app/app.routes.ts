c18ca299fe7119b8613a2e4731fd51cada4fc5cb
antur84 <andreas.turku@gmail.com>
2024-01-09 11:32:09 +0100

    feat(main-menu-page.component) added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,4 @@
 import { Routes } from '@angular/router';
+import { MainMenuPageComponent } from './pages/main-menu-page/main-menu-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{ component: MainMenuPageComponent, path: '' }];
---
+++ src/app/pages/main-menu-page/main-menu-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-menu-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './main-menu-page.component.html',
+  styleUrl: './main-menu-page.component.scss',
+})
+export class MainMenuPageComponent {}
